fa7e2a47828bab2f8226d585b01c83b4ce85fd5b
Akshat Trivedi <akshattriv.iit@gmail.com>
2021-11-29 16:35:53 +0530

    chore: finished UI for welcome page - login and signup

---
+++ components/coreComponents/InputText.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="inputTextWrapper">
+    <input
+      :id="componentId"
+      :type="componentType"
+      :name="componentName"
+      class="inputTextField"
+      :placeholder="componentPlaceholder"
+      :value="value"
+      :required="required"
+      @input="$emit('input', $event.target.value)"
+    />
+    <label :for="componentId" class="inputTextLabel">{{
+      componentPlaceholder
+    }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputText',
+  props: {
+    componentId: {
+      type: String,
+      required: true,
+    },
+    componentName: {
+      type: String,
+      required: true,
+    },
+    componentPlaceholder: {
+      type: String,
+      required: true,
+    },
+    componentType: {
+      type: String,
+      default: 'text',
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+    required: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.inputTextField {
+  border: none;
+  outline: none;
+  padding: 1rem;
+  width: 100%;
+  height: 100%;
+  font-size: 1.6rem;
+  background: transparent;
+  position: relative;
+  z-index: 2;
+
+  &::placeholder {
+    color: var(--text2);
+  }
+}
+
+.inputTextWrapper {
+  background: var(--surface2);
+  width: 30rem;
+  border-radius: 6px;
+  position: relative;
+
+  &:not(:last-of-type) {
+    margin-bottom: 3rem;
+  }
+}
+
+.inputTextLabel {
+  position: absolute;
+  z-index: 1;
+  left: 0;
+  top: 50%;
+  opacity: 1;
+  transform: translate(1rem, 110%);
+  transition: transform 0.3s, opacity 0.2s ease-in;
+  color: var(--text2);
+  font-size: 1.4rem;
+  font-weight: 600;
+}
+
+.inputTextField:placeholder-shown + .inputTextLabel {
+  transform: translate(1rem, -50%);
+  opacity: 0;
+}
+</style>
--- components/loginPageComponents/LoginPageRight.vue
+++ components/loginPageComponents/LoginPageRight.vue
@@ -1,10 +1,101 @@
 <template>
-  <div class="loginPageRight"></div>
+  <div class="loginPageRight">
+    <div class="loginSignupWrapper">
+      <h1 class="loginHeading">
+        {{ signupActive ? 'Signup for free now' : 'Login into your account' }}
+      </h1>
+      <input-text
+        v-if="signupActive"
+        v-model="user.name"
+        component-name="name"
+        component-placeholder="Name"
+        component-id="userName"
+      ></input-text>
+      <input-text
+        v-model="user.email"
+        component-name="email"
+        component-placeholder="Email"
+        component-id="userEmail"
+        component-type="email"
+      ></input-text>
+      <input-text
+        v-model="user.password"
+        component-name="password"
+        component-placeholder="Password"
+        component-id="password"
+        component-type="password"
+      ></input-text>
+      <input-text
+        v-if="signupActive"
+        v-model="user.passwordConfirm"
+        component-name="passwordConfirm"
+        component-placeholder="Password Confirm"
+        component-id="passwordConfirm"
+        component-type="password"
+      ></input-text>
+
+      <span class="forgotPassword highlight underline" @click="forgotPassword">
+        forgot password?
+      </span>
+
+      <button class="submitBtn" @click="submit">
+        {{ signupActive ? 'Signup' : 'Login' }}
+      </button>
+    </div>
+    <span class="bottomText">
+      {{ signupActive ? 'Already have an account?' : 'Create an account,' }}
+      <span class="highlight underline" @click="changeSignup">
+        {{ signupActive ? 'Login' : 'Signup' }}
+      </span>
+    </span>
+  </div>
 </template>
 
 <script>
+import { ref, reactive } from '@nuxtjs/composition-api'
+import InputText from '@/components/coreComponents/InputText.vue'
+
 export default {
   name: 'LoginPageRight',
+  components: {
+    InputText,
+  },
+  setup() {
+    const signupActive = ref(false)
+    const user = reactive({
+      email: '',
+      password: '',
+      name: '',
+      passwordConfirm: '',
+    })
+
+    function changeSignup() {
+      signupActive.value = !signupActive.value
+
+      user.email = ''
+      user.name = ''
+      user.password = ''
+      user.passwordConfirm = ''
+    }
+
+    function submit() {
+      // eslint-disable-next-line no-console
+      console.log(user.name, user.email, user.password, user.passwordConfirm)
+    }
+
+    function forgotPassword() {
+      // eslint-disable-next-line no-console
+      console.log('forgot password')
+    }
+
+    return {
+      signupActive,
+      user,
+      changeSignup,
+      submit,
+      forgotPassword,
+    }
+  },
 }
 </script>
 
@@ -13,5 +104,65 @@ export default {
   height: 100%;
   background: var(--surface3);
   flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+
+.loginHeading {
+  font-size: 1.8rem;
+  color: var(--text1);
+  text-align: center;
+  font-weight: 400;
+  margin-bottom: 3rem;
+}
+
+.submitBtn {
+  width: 20rem;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10rem;
+  margin: 4rem auto 0;
+  background: var(--surface2);
+  border: none;
+  outline: none;
+  box-shadow: 0 0.2rem 0.4rem rgba(0, 0, 0, 0.1);
+
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.bottomText {
+  position: absolute;
+  bottom: 1rem;
+  left: 50%;
+  transform: translateX(-50%);
+  width: max-content;
+  display: block;
+  font-size: 1.4rem;
+}
+
+.highlight {
+  color: var(--brand);
+  display: inline-block;
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.forgotPassword {
+  display: inline-block;
+  margin-top: 1rem;
+  font-size: 1.5rem;
+  width: 100%;
+  text-align: right;
 }
 </style>
